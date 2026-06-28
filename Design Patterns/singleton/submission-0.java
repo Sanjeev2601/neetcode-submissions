8bc5016890b5b35adc64646127c261a47405223e
Sanjeev Parthasarathy <sanjeev.p2601@gmail.com>
2026-06-27 22:11:00 -0500

    Add: singleton - submission-0

---
+++ Design Patterns/singleton/submission-0.java
@@ -0,0 +1,22 @@
+static class Singleton {
+    private String val;
+    private static Singleton uniqueInstance = null;
+    private Singleton() {
+        val = "";
+    }
+
+    public static Singleton getInstance() {
+        if(uniqueInstance == null)
+            uniqueInstance = new Singleton();
+        return uniqueInstance;
+    }
+
+    public String getValue() {
+        return val;
+    }
+
+    public void setValue(String value) {
+        val = value;
+    }
+    
+}
